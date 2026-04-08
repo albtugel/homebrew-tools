1871ba1d56d67e3211107ac721a92c1a019f705a
Alibek Tugelbayev <al.tugel02@gmail.com>
2026-04-08 12:59:03 +0500

    feat: bump xct to v1.0.3 with PATH fix

--- Formula/xct.rb
+++ Formula/xct.rb
@@ -1,12 +1,12 @@
 class Xct < Formula
   desc "Automation scripts for Xcode projects"
   homepage "https://github.com/albtugel/xcode-cli-tools"
-  url "https://github.com/albtugel/xcode-cli-tools/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "a1bda0f0ea514e2dd9b42696d29721922023e11dd24a74af4ec90ace05237b3a"
+  url "https://github.com/albtugel/xcode-cli-tools/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "abb33095f1214e52e7598adf7f346a91a547ad98fedae1d8a1fd737469943c85"
   license "MIT"
 
   def install
-    # Устанавливаем файлы сразу как готовые команды в bin
+
     bin.install "OpenXcodeProj.zsh" => "xproj"
     bin.install "RunXcodeProj.zsh" => "xrun"
     bin.install "TestXcodeProj.zsh" => "xtest"
